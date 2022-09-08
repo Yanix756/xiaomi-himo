b4c50f4c7100820a1b2cec6bb6f94ef57b7ac82f
Yanix756 <80466272+Yanix756@users.noreply.github.com>
2022-09-08 14:22:04 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function(){
+
+    $('.header__slider-inner').slick({
+        dots: true,
+        arrows: false,
+        autoplay:3000,
+        fade:true,
+    });
+
+    $('.slider__inner').slick({
+        dots: true,
+        arrows: false,
+        autoplay:3000,
+        fade:true,
+    });
+
+    
+});
